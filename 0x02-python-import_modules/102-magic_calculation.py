8136fac040cd686d486cc56e00a7a9a62e4d65c4
Jima-J9M1 <Jimd3730@gmail.com>
2022-06-02 17:36:11 +0300

    upload file

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
 #!/usr/bin/python3
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
@@ -9,4 +8,3 @@ def magic_calculation(a, b):
         return (c)
     else:
         return (sub(a, b))
-
