ebd98104e356e04c5dc193b3244f4eebf25ddd0a
Jima-J9M1 <Jimd3730@gmail.com>
2022-06-28 22:23:39 +0300

    upload

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - check if the linked list is cycle
+ * @head: pointer to the list that can be checked
+ * Return: Return integer
+ */
+int check_cyle(listint_t *list)
+{
+    if (list == NULL)
+        return (0);
+    return (1);
+}
